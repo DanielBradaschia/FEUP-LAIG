01251fff19a47470186ed3d180495979815f934c
Daniel Gazola Bradaschia <daniel.bradaschia@gmail.com>
2019-11-04 15:52:38 +0000

    Update KeyframeAnimation.js

--- TP2/KeyframeAnimation.js
+++ TP2/KeyframeAnimation.js
@@ -1,10 +1,16 @@
 class KeyFrameAnimation extends Animation {
-    constructor(scene, animation_id, instant, translation, rotation, scale) {
-        this.scene = scene;
-        this.id = animation_id;
+    constructor(scene, animation_id, instant, begin, translation, rotation, scale) {
+        super(scene, animation_id, instant-begin);        
         this.instant = instant;
         this.transMat = translation;
         this.rotMat = rotation;
         this.scaleMat = scale;
+    }
+    display() {
+
+    }
+
+    update() {
+
     }
 }
